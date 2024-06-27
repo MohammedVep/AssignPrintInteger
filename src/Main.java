0ee4f4549e41e48373ef395dedec46c2762d46cd
MohammedVep <98326062+MohammedVep@users.noreply.github.com>
2024-06-27 15:05:00 -0400

    Completed Assign and Print integer values exercise

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        int a;
+        int b;
+        a = 54;
+        b = 35;
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
