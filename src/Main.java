3cd736ea8d4a53b96f5b341d02a5bdd75e98cb44
MohammedVep <98326062+MohammedVep@users.noreply.github.com>
2024-06-27 15:06:50 -0400

    find efficencies

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,8 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        int a;
-        int b;
-        a = 54;
-        b = 35;
+        int a = 54;
+        int b = 35;
         System.out.println(a);
         System.out.println(b);
     }
